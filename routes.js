2be500163b7f1cd5c1eef2aeeebafd7e391a2238
Greg <greggordon98@gmail.com>
2019-06-26 13:12:45 -0700

    learn
    learn

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const { check, validationResult } = require('express-validator/check');
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
 
 // This array is used to keep track of user records
 // as they are created.
@@ -13,11 +15,51 @@ const nameValidator = check('name')
   .exists({ checkNull: true, checkFalsy: true })
   .withMessage('Please provide a value for "name"');
 
+const authenticateUser = (req, res, next) => {
+    // Parse the user's credentials from the Authorization header.
+    const credentials = auth(req);
+
+    // If the user's credentials are available...
+    if(credentials) {
+    // Attempt to retrieve the user from the data store
+    // by their username (i.e. the user's "key"
+    // from the Authorization header).
+    const user = users.find(u => u.username === credentials.name);
+
+
+    // If a user was successfully retrieved from the data store...
+        if(users) {
+    // Use the bcryptjs npm package to compare the user's password
+    // (from the Authorization header) to the user's password
+    // that was retrieved from the data store.
+            const authenticated = bcryptjs
+                .compareSync(credentials.pass, user.password);
+
+    // If the passwords match...
+            if(authenticated) {
+    // Then store the retrieved user object on the request object
+    // so any middleware functions that follow this middleware function
+    // will have access to the user's information.
+                req.currentUser = user;
+            }
+        }
+    }
+
+    // If user authentication failed...
+    // Return a response with a 401 Unauthorized HTTP status code.
+
+    // Or if user authentication succeeded...
+    // Call the next() method.
+    next();
+}
+
+router.get('/users', authenticateUser, (req, res) => {
+    // Code to get and return the current user...
+  });
+
 // Route that creates a new user.
 router.post('/users', [
-    check('name')
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Please provide a value for "name"'),
+    nameValidator,
     check('username')
       .exists({ checkNull: true, checkFalsy: true })
       .withMessage('Please provide a value for "username"'),
@@ -39,6 +81,9 @@ router.post('/users', [
 
   // Get the user from the request body.
   const user = req.body;
+
+  // Hash the new user's password.
+  user.password = bcryptjs.hashSync(user.password);
 
   // Add the user to the `users` array.
   users.push(user);
